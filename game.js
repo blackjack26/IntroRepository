d7f76ad49f57a3f1e1780fc2be53c7ddcbbc019c
Jack Grzechowiak <johngrzechowiak26@gmail.com>
2014-11-13 16:48:54 -0500

    Added key events

--- game.js
+++ game.js
@@ -1,25 +1,86 @@
-
-var click = function(){
-	if(document.getElementById('action').value == "Scream!"){
-		document.getElementById('descrip').innerHTML = "HA, you could've ran, but you wasted your time screaming";
-	}
-}	
-var runSomeTests = function(){
-	var helpList = document.querySelector('#help > ul');
-
-	var actionRun = document.createElement("LI");
-	actionRun.innerHTML = "Run";
-	helpList.appendChild(actionRun);
-
-	var actionRun = document.createElement("LI");
-	actionRun.innerHTML = "Look";
-	helpList.appendChild(actionRun);
-
-	var actionRun = document.createElement("LI");
-	actionRun.innerHTML = "Scream!";
-	helpList.appendChild(actionRun);
+function Player(name){
+	this.name = name;
+	this.items = [];
+	this.actions = ["pickup", "drop"];
 }
-window.onload = runSomeTests;
+Player.prototype.pickup = function(item){
+	this.items.push(item);
+}
+Player.prototype.drop = function(item){
+	var index = this.items.indexOf(item);
+	if(index >= 0){
+		this.items.splice(index,1);
+		
+		var inventory = document.querySelector("#inventory > ul");
+		var inventoryItems = inventory.childNodes;
+		for(i in inventoryItems){
+			if(inventoryItems[i].textContent == item){
+				inventory.removeChild(inventoryItems[i]);
+				break;
+			}
+		}
+	}
+}
 
-document.getElementById("button").onclick = click;
-document.getElementsByTagName("h1")[0].innerHTML = "The Horrors of Marist";
+function updateActions(str){
+	for(i in player.actions){
+		if(player.actions[i].startsWith(str)){
+			var help = document.querySelector("#help > ul");
+			var helpItems = help.childNodes;
+			for(i in helpItems){
+				if(helpItems[i].textContent == actions[i]){
+					helpItems[i].style.color = "red";
+				}
+			}
+		}
+	}
+}
+
+function report(){
+	var inventory = document.querySelector("#inventory > ul");
+	for(i in player.items){
+		var liElement = document.createElement("LI");
+		liElement.textContent = player.items[i];
+		inventory.appendChild(liElement);
+	}
+	
+	var help = document.querySelector("#help > ul");
+	for(i in player.actions){
+		var liElement = document.createElement("LI");
+		liElement.textContent = player.actions[i];
+		help.appendChild(liElement);
+	}
+}
+
+function interpret(str){
+	var obj = {};
+	var strArray = str.split(" ");
+	obj.action = strArray[0];
+	strArray.shift();
+	obj.object = strArray.join(" ");
+	return obj;
+}
+
+function execute(obj){
+	player[obj.action](obj.object);
+}
+
+function gameStep(str){
+	var obj = interpret(str);
+	execute(obj);
+	report();
+}
+
+function gameStart(){
+	report();
+	var textbox = document.getElementById("action");
+	textbox.addEventListener("keyup", function(event){
+		if(event.keyCode == 13){
+			gameStep(textbox.value);
+		}
+		updateActions(textbox.value);
+	});
+}
+
+var player = new Player("Jack");
+window.onload = gameStart;

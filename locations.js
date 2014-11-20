7934c1938d99bf0f8a3986650b8b708fd7326e34
Jack Grzechowiak <johngrzechowiak26@gmail.com>
2014-11-20 16:04:56 -0500

    location javascript file addition with matrix

---
+++ locations.js
@@ -0,0 +1,31 @@
+function Location(name, description){
+	this.name = name;
+	this.description = description;
+	this.items = [];
+}
+
+var loc0 = new Location("Location 0", "small box-like room with no windows");
+var loc1 = new Location("Location 1", "large circular room");
+var loc2 = new Location("Location 2", "outside and it is a nice day");
+
+var locations = [loc0, loc1, loc2];
+
+var connections = 
+	[[0, 1, 1],
+	 [1, 0, 1],
+	 [1, 1, 0]];
+
+var map = {
+	locs: locations,
+	connect: connections
+}
+
+var getAdjacentLocations = function(locNum){
+	for(var i = 0; i < map.connect.length; i++){
+		if(map.connect[locNum][i] == 1){
+			console.log(locations[i].name);
+		}
+	}
+}
+
+getAdjacentLocations(2);
